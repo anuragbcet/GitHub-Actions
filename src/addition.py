4617ef968d3cdb8ec9814df75b7bbc6e83b87aad
Anurag Kumar <anuragbcet2023@gmail.com>
2025-10-17 11:03:32 +0530

    added substraction function and tests

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,18 @@
 # app.py
 # This is a test commit
+
+#Anurag is making some changes 
 def add(a, b):
     return a + b
+
+def sub(a, b):
+    return a - b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+
+def test_sub():
+    assert sub(5, 2) == 3
+    assert sub(1, 2) == -1
